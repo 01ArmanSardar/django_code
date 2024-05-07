d631aa08b875cd27a38c1fc884996d80b9ff65d0
ArmanSardar <mdarmansardar57@gmail.com>
2024-05-08 01:15:50 +0600

    a little bit change on musician directory project

--- Module 15.5 Practice day 01/Musicians Directory/musician/urls.py
+++ Module 15.5 Practice day 01/Musicians Directory/musician/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   path('',views.home,name='musucianPage')
+   path('',views.home,name='musucianPage'),
+   path('editMusician/<int:id>',views.edit_musician,name='editmusucian')
 ]
