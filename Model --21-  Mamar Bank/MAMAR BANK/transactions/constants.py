9ef813ba0fb05152cf7d0a377511cb64e28f14db
ArmanSardar <mdarmansardar57@gmail.com>
2024-06-12 21:44:43 +0600

    withdraw,deposit,LoanRequest view

--- Model --21-  Mamar Bank/MAMAR BANK/transactions/constants.py
+++ Model --21-  Mamar Bank/MAMAR BANK/transactions/constants.py
@@ -1,7 +1,11 @@
+DEPOSIT=1
+WITHDRAWL=2
+LOAN=3
+LOAN_PAID=4
 TRANSACTION_TYPE=(
-    (1,'Deposit'),
-    (2,'Withdrawal'),
-    (3,'Loan'),
-    (4,'Loan Paid'),
+    (DEPOSIT,'Deposit'),
+    (WITHDRAWL,'Withdrawal'),
+    (LOAN,'Loan'),
+    (LOAN_PAID,'Loan Paid'),
 
 )
--- Model --21-  Mamar Bank/MAMAR BANK/transactions/views.py
+++ Model --21-  Mamar Bank/MAMAR BANK/transactions/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Transaction
+from .forms import DepositForm,WithdrawForm,LoanRequestForm
+from django.contrib import messages
+from .constants import DEPOSIT,WITHDRAWL,LOAN,LOAN_PAID
 # Create your views here.
 
 class TransactionCreateMixin(LoginRequiredMixin,CreateView):
@@ -25,3 +28,59 @@ class TransactionCreateMixin(LoginRequiredMixin,CreateView):
               'title'  :self.title
             }
         )
+
+class DepositMoneyView(TransactionCreateMixin):
+    form_class=DepositForm
+    title="Deposit"
+
+    def get_initial(self):
+        inital={'transaction_type':DEPOSIT}
+        return inital
+    
+    def form_valid(self,form):
+        amount=form.cleaned_data.get('amount')
+        account=self.request.user.account
+        account.balance+=amount#user er kaceh ache 500 taka ,ami deposit korlam 1000 tk taile total balance hocche 1500
+        account.save(
+            update_fields=['balance']
+        )
+        messages(self.request,f'{amount} was deposited to your account')
+        return super().form_valid(form)
+    
+
+class WithdrawMoneyView(TransactionCreateMixin):
+    form_class=WithdrawForm
+    title="WithdrawForm"
+
+    def get_initial(self):
+        inital={'transaction_type':WITHDRAWL}
+        return inital
+    
+    def form_valid(self,form):
+        amount=form.cleaned_data.get('amount')
+        account=self.request.user.account
+        account.balance-=amount#user er kaceh ache 1500 taka ,ami withdraw korlam 1000 tk taile total balance hocche akhn 500
+        account.save(
+            update_fields=['balance']
+        )
+        messages(self.request,f'{amount} was withdraw to your account')
+        return super().form_valid(form)
+    
+
+
+
+class LoanRequestView(TransactionCreateMixin):
+    form_class=LoanRequestForm
+    title="Request for Loan"
+
+    def get_initial(self):
+        inital={'transaction_type':LOAN}
+        return inital
+    
+    def form_valid(self,form):
+        amount=form.cleaned_data.get('amount')
+        current_loan_count=Transaction.objects.filter(account=self.request.user.account,transaction_type=3,loan_approve=True).count()
+        if current_loan_count >=3:
+            return HttpResponse("you have crossed your limits")
+        messages(self.request,f'loan request for{amount} was succesfully sent in admin')
+        return super().form_valid(form)
